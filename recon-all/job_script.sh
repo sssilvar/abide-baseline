b44adbbc1b1723a446cdecd03dbc66d1f6db5adf
Santiago <sssilvar@unal.edu.co>
2020-09-18 11:02:29 +0200

    New version

--- recon-all/job_script.sh
+++ recon-all/job_script.sh
@@ -6,6 +6,21 @@
 
 FSDIR='/data/epione/user/ssilvari/RAW_DATASETS/ABIDE-I_FS'
 
+if [ -z "$1" ]; then
+    echo "First argument needed: T1 input file."
+    exit 1
+fi
+
+if [ ! -f "$1" ]; then
+    echo "File does not exist: ${1} ."
+    exit 1
+fi
+
+if [ -z "$2" ]; then
+    echo "Second argument needed: Subject ID"
+    exit 1
+fi
+
 recon-all -i $1 -s $2 -sd "/dev/shm" -all
 
 pid=$!
