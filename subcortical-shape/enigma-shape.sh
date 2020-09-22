f7997febb2a1430cfbcc2b35487732dad0ee88f1
Santiago <sssilvar@unal.edu.co>
2020-09-22 11:42:37 +0200

    New version

---
+++ subcortical-shape/enigma-shape.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+#OAR -p mem_core>8000
+#OAR -l core=1,walltime=30:00:00
+#OAR -O /home/ssilvari/logs/%jobid%.output
+#OAR -E /home/ssilvari/logs/%jobid%.error
+
+MEDIAL_DEMONS_DIR=${MEDIAL_DEMONS_DIR:="/home/ssilvari/Apps/MedialDemonsShared"}
+SUBJECT_ID=$1
+
+if [ -z "$SUBJECT_ID" ]; then
+    echo "First argument needed: Subject ID has not been provided."
+    exit 1
+fi
+
+# perl MedialDemonsShared/bin/Medial_Demons_shared.pl /local/freesurfer/subjects/bert/mri/aseg.mgz 26 58 /home/ssilvari/Downloads/shape MedialDemonsShared/ /local/freesurfer/bin
+ROIS="10 11 12 13 17 18 26 49 50 51 52 53 54 58"
+SUBJECT_FOLDER="${SUBJECTS_DIR}/${SUBJECT_ID}"
+
+CMD="perl ${MEDIAL_DEMONS_DIR}/bin/Medial_Demons_shared.pl ${SUBJECT_FOLDER}/mri/aseg.mgz ${ROIS} ${SUBJECT_FOLDER}/shapes ${MEDIAL_DEMONS_DIR} ${FREESURFER_HOME}/bin"
+echo "${CMD}"
+eval "${CMD}"
---
+++ subcortical-shape/launch_all_jobs.sh
@@ -0,0 +1,30 @@
+#!/bin/sh
+
+export SUBJECTS_DIR='/data/epione/user/ssilvari/RAW_DATASETS/ABIDE-I_FS'
+export MEDIAL_DEMONS_DIR="/home/ssilvari/Apps/MedialDemonsShared"
+
+CURRENT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+for path in $(find "${SUBJECTS_DIR}"  -name "aseg.mgz")
+do
+  # Extract Subject ID from image SID.nii.gz
+  sid=$(echo "${path}" | awk -F/ '{print $(NF-2)}')
+  results_directory="$SUBJECTS_DIR/$sid/shapes"
+
+  # Check if directory does not exist. If it does, skip the job launching (less karma)
+	if [[ ! -d $results_directory ]]
+	then
+	  echo "Launching job for subject ${sid}"
+		oarsub -S "${CURRENT_DIR}/enigma-shape.sh $sid"
+  else
+    echo "Subject in folder ${results_directory} was already processed."
+	fi
+
+  # Limit the number of jobs to 500
+	n_jobs=$(oarstat -u | wc -l)
+	while [ "${n_jobs}" -gt 500 ]
+	do
+		sleep 1m
+		n_jobs=$(oarstat -u | wc -l)
+	done
+done
